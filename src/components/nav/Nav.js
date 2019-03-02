1cef56e719233190d3eb2eb2325e84f70ef8440b
cris93g <37388115+cris93g@users.noreply.github.com>
2019-03-02 17:41:51 -0600

    started proj

---
+++ src/components/nav/Nav.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+  width: 100vw;
+  margin: auto;
+  display: flex;
+  height: 15vh;
+  align-items: center;
+  background-color: red;
+`;
+const Line = styled.section`
+  width: 100%;
+
+  display: flex;
+  background-color: lightgray;
+  height: 1px;
+`;
+const BottomBorder = styled.section`
+  width: 100%;
+
+  display: flex;
+  background-color: red;
+  height: 8vh;
+`;
+const LeftBottom = styled.section`
+  width: 50%;
+  display: flex;
+  height: 100%;
+  flex-direction: row;
+  align-items: stretch;
+  align-content: stretch;
+`;
+const RightBottom = styled.section`
+  width: 50%;
+  display: flex;
+  height: 100%;
+`;
+export default class Nav extends Component {
+  render() {
+    return (
+      <div>
+        <Wrapper />
+        <Line />
+        <BottomBorder>
+          <LeftBottom>
+            {/* <div>
+              <div>Products</div>
+              <div>Brands</div>
+              <div>Deals</div>
+              <div>Services</div>
+            </div> */}
+          </LeftBottom>
+          <RightBottom>
+            {" "}
+            <div />
+          </RightBottom>
+        </BottomBorder>
+      </div>
+    );
+  }
+}
